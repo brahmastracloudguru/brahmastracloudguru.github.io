94806178d4207a12f9b049c82190c9730c7600d3
Srinivas T <salesforce.guru@icloud.com>
2024-12-04 10:11:54 +0530

    Admin Course Content Sample

---
+++ ultimateAdmin.js
@@ -0,0 +1,5 @@
+// Add any interactivity here if needed.
+// Example: Alert on link click
+document.querySelector('.register-link').addEventListener('click', () => {
+    alert('Redirecting to registration page...');
+});
